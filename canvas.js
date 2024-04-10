fb5869c0688baaef66161b47b4737eba48b04d8c
Lisandro Tecles <ltecles@gmail.com>
2024-04-10 19:27:06 -0300

    .

---
+++ canvas.js
@@ -0,0 +1,45 @@
+let elemento=document.getElementById("lienzo");
+let lienzo=elemento.getContext('2d');
+lienzo.beginPath();
+
+/*Marco*/
+lienzo.arc(100, 100, 100, Math.PI, 1.5*Math.PI);
+lienzo.arc(400, 100, 100, 1.5*Math.PI, 0);
+lienzo.arc(400, 400, 100, 0, 0.5*Math.PI);
+lienzo.arc(100, 400, 100, 0.5*Math.PI, Math.PI);
+
+/*lienzo.lineTo(450,0);*/
+lienzo.fillStyle="#F7F7F7"
+lienzo.fill();
+lienzo.beginPath();
+
+/*Orejas*/
+lienzo.ellipse(100, 100, 85, 95, 10, 0, 2 * Math.PI);
+lienzo.fillStyle="black"
+lienzo.fill();
+
+lienzo.ellipse(400, 100, 85, 95, -10, 0, 2 * Math.PI);
+lienzo.fillStyle="black"
+lienzo.fill();
+
+/*Cabeza*/
+lienzo.beginPath();
+lienzo.arc(250, 250, 150, 0.8*Math.PI, 0.2*Math.PI);
+lienzo.fill();
+
+
+/*Ojos*/
+lienzo.beginPath();
+
+lienzo.ellipse(200, 220, 65, 90, 0, 0, 2 * Math.PI);
+lienzo.fillStyle="#FDE3CA";
+lienzo.fill();
+
+
+
+lienzo.ellipse(300, 220, 65, 90, 0, 0, 2 * Math.PI);
+lienzo.fillStyle="#FDE3CA";
+lienzo.fill();
+/*
+lienzo.moveTo(0,0);
+lienzo.lineTo(450,0)*/
